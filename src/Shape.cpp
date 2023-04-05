eb73ff05330a8a346fc25459d5a3335bc0d40121
Blake <blakeavery42@tamu.edu>
2023-04-04 23:49:25 -0500

    Output min y-values

--- src/Shape.cpp
+++ src/Shape.cpp
@@ -81,7 +81,7 @@ void Shape::loadMesh(const string &meshName)
 		vmax.z = max(vmax.z, v.z);
 	}
 	minY = vmin.y;
-	//std::cout << vmin.y << std::endl;
+	std::cout << "Mesh name: " << meshName << ", Min y value: " << vmin.y << std::endl;
 }
 
 void Shape::fitToUnitBox()
